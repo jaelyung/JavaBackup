d6e5fb33955a85a581948cd6032fbba8e1102372
HUu-11 <HUu-11@HUu-11-PC>
2020-03-24 17:17:54 +0900

    3.24클래스와인스턴스

---
+++ src/com/human/ex/ClassStart03.java
@@ -0,0 +1,11 @@
+package com.human.ex;
+
+public class ClassStart03 {
+
+	public static void main(String[] args) {
+		// 1.블랙잭을 객체로 바꾸고 5번 경기한 내용을 출력
+		// 2.은행에서 100명의 사용자의 관리하는 프로그램 ->객체로 만들어 100개의 배열, 사용자 ID와 비밀번호를 입력받는 필드를만든다.
+
+	}
+
+}
